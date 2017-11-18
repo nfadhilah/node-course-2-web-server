667de1ed58cddead3fe9a59122f15d23f45b9922
Mono Noor Fadhilah <halilintar@gmail.com>
2017-11-18 15:59:41 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const maitenanceMode = true;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.path} \n`
+
+  fs.appendFile('server.log', log, (error) => {
+    if (error) {
+      console.log('Unable to append server.log');
+    }
+  });
+
+  console.log(log);
+
+  next();
+});
+
+if (maitenanceMode) {
+  app.use((req, res, next) => {
+    res.render('maintenance.hbs', {
+      pageTitle: 'Will be right back',
+      message: 'The site is currently being updated.'
+    });
+  });
+}
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
